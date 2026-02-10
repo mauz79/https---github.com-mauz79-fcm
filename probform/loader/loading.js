50530e5b117eab9a28fb516da15c8bfddce14b23
mauz79 <84195308+mauz79@users.noreply.github.com>
2026-02-10 23:54:12 +0100

    Delete probform directory

--- probform/loader/loading.js
+++
@@ -1,8 +0,0 @@
-
-document.write("<table class='loading' border='0' cellspacing='0' cellpadding='0' summary=''>");
-document.write("<tr><td class='intestazione' colspan='2'>");
-document.write("Generazione Dati Probabili Formazioni");
-document.write("</td></tr><tr>");
-document.write("<td id='loadingFCC' class='loading'><img alt='' src='probform/loader/loading.gif' id='LoadingFc'/></td>");
-document.write("<td id='quotidianoFCC' class='quotidiano'><span id='Fc'>Lettura dati...</span></td>");
-document.write("</tr></table>");
